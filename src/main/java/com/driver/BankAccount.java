b385ab6549a68dea7eb1b69eb51571da76a222c0
Kumar Devendra <mjrajak47z@gmail.com>
2023-03-29 15:33:30 +0530

    Banking Assignment

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -7,25 +7,75 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
+              this.name = name;
+              this.balance = balance;
+              this.minBalance = minBalance;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-
-        return null;
+        if(digits*9 < sum){
+            throw new Exception("Account Number can not be generated");
+        }
+        else{
+            StringBuilder accountNumberBuilder = new StringBuilder();
+            int remainingSum = sum;
+            for(int i=0; i<digits; i++){
+                if(remainingSum >= 9){
+                    accountNumberBuilder.append(9);
+                    remainingSum-=9;
+                }
+                else if(remainingSum > 0){
+                    accountNumberBuilder.append(remainingSum);
+                    remainingSum = 0;
+                }
+                else{
+                    accountNumberBuilder.append(0);
+                }
+            }
+            return accountNumberBuilder.toString();
+        }
     }
 
     public void deposit(double amount) {
         //add amount to balance
+        balance = balance + amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+         if(balance-amount < minBalance){
+            throw new Exception("Insufficient Balance");
+         }
+         else{
+             balance -= amount;
+         }
     }
-
 }
